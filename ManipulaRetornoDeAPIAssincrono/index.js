d17dd2acfe98c2f228d774b0e9723e9c985dfae2
eduardoworrel <eduardo.worrel@gmail.com>
2021-12-01 21:38:50 -0300

    utilizando promise.all

--- ManipulaRetornoDeAPIAssincrono/index.js
+++ ManipulaRetornoDeAPIAssincrono/index.js
@@ -1,5 +1,5 @@
-const body = document.getElementsByTagName("body")[0];
 
+const body = document.getElementsByTagName("body")[0];
 let numero = window.prompt("Digite um número de 1 a 10");
 
 if(numero > 0 && numero <= 10){
@@ -8,25 +8,30 @@ if(numero > 0 && numero <= 10){
     alert("número invalido, por padrão será escolhido o 5");
     numero = 5;
 }
-
+const returnJson = (response) => response.json();
 for(let i = 1; i <= numero; i++){
-    let sections;
-    fetch("https://dog.ceo/api/breeds/image/random").then( function(data){
-        return data.json();
-    }).then(function(json){
-        const resultado = json.message;
-        const section = "<section style=\"background-image:url('"+resultado+"')\">dog</section>";
-        body.insertAdjacentHTML("beforeend",section)
-    })
 
-    fetch("https://api.thecatapi.com/v1/images/search").then(function(data){
-        return data.json();
-    }).then(function(json){
-        const resultado = json[0].url;
-        const section = "<section style=\"background-image:url('"+resultado+"')\">cat</section>";
-        body.insertAdjacentHTML("beforeend",section)
-        
-    })
+    let sections;
+    
+    let promessaDeRetornoJsonDog = fetch("https://dog.ceo/api/breeds/image/random")
+    .then(returnJson);
+    let promessaDeRetornoJsonCat = fetch("https://api.thecatapi.com/v1/images/search")
+    .then(returnJson)
+
+    Promise.all([promessaDeRetornoJsonDog , promessaDeRetornoJsonDog]).then((values)=>{
+        for(item of values){
+            if(Object.prototype.toString.call(item) == "[object Object]"){
+                const resultado = json.message;
+                const section = "<section style=\"background-image:url('"+resultado+"')\">dog</section>";
+                body.insertAdjacentHTML("beforeend",section)
+            }else{
+                const resultado = json[0].url;
+                const section = "<section style=\"background-image:url('"+resultado+"')\">cat</section>";
+                body.insertAdjacentHTML("beforeend",section)
+            }
+        }
+
+    });
 }
 
 
